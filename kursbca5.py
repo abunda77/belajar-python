c53fd7fe47df0bb7f4a4084cd497fa438d0d73b6
Erie Putranto <erieputranto@gmail.com>
2023-07-01 18:43:54 +0700

    Update kurs bca 5

--- kursbca5.py
+++ kursbca5.py
@@ -14,7 +14,7 @@ def print_table(rows):
     
     # Cetak tabel
     print("\033[94m+" + "-" * (col_widths[0]+2) + \
-          "+" + "-" * (col_widths[1]+2) + \
+          "+" + "-" * (col_widths[1]+12) + \
           "+" + "-" * (col_widths[2]+2) + "+\033[0m")
     for row in rows:
         print("\033[94m|\033[0m", end="")
@@ -26,29 +26,25 @@ def print_table(rows):
                 print(f"\033[91m{cell}\033[0m{padding}\t\033[94m|\033[0m", end="")
         print()
     print("\033[94m+" + "-" * (col_widths[0]+2) + \
-          "+" + "-" * (col_widths[1]+2) + \
+          "+" + "-" * (col_widths[1]+12) + \
           "+" + "-" * (col_widths[2]+2) + "+\033[0m")
 
 
-    
-def format_currency(amount, currency):
-    if currency == "IDR":
-        return "Rp {:,.0f}".format(amount).replace(".", ",")
-    else:
-        return "${:,.2f}".format(amount)
+import pyfiglet
+from termcolor import colored
 
 def format_currency(amount, currency):
     if currency == "IDR":
-        return "Rp {:,.0f}".format(amount).replace(".", ",")
+        return colored(f"Rp {amount:,.0f}", "green").replace(".", ",")
     else:
-        return "${:,.2f}".format(amount)
+        return colored(f"${amount:,.2f}", "blue")
 
 def calculator(rows):
-    print("Pilih jenis konversi:")
-    print("1. IDR ke USD")
-    print("2. USD ke IDR")
-    print("3. Kembali")
-    choice = input("Masukkan pilihan Anda: ")
+    print(colored("Pilih jenis konversi:", "yellow"))
+    print(colored("1. IDR ke USD", "cyan"))
+    print(colored("2. USD ke IDR", "cyan"))
+    print(colored("3. Kembali", "cyan"))
+    choice = input(colored("Masukkan pilihan Anda: ", "magenta"))
     if choice.isdigit() and 1 <= int(choice) <= 3:
         choice = int(choice)
         if choice == 3:
@@ -60,22 +56,22 @@ def calculator(rows):
             else:
                 from_currency = "USD"
                 to_currency = "IDR"
-            print(f"Anda memilih konversi {from_currency} ke {to_currency}")
-            print(f"Masukkan jumlah {from_currency}:")
-            amount = input(f"{from_currency} ")
+            print(colored(f"Anda memilih konversi {from_currency} ke {to_currency}", "yellow"))
+            print(colored(f"Masukkan jumlah {from_currency}:", "magenta"))
+            amount = input(colored(f"{from_currency} ", "blue"))
             if amount.replace(".", "", 1).isdigit():
                 amount = float(amount.replace(",", ""))
                 if from_currency == "IDR":
-                    nilai_jual = float(rows[0][2].replace(",", ""))
-                    hasil = amount / nilai_jual
+                    nilai_jual = float(rows[0][1].replace(",", ""))
+                    hasil = amount / (nilai_jual * 1000)
                 else:
-                    nilai_beli = float(rows[0][1].replace(",", ""))
-                    hasil = amount * nilai_beli
-                print(f"Jumlah {from_currency} {format_currency(amount, from_currency)} = {to_currency} {format_currency(hasil, to_currency)}")
+                    nilai_beli = float(rows[0][2].replace(",", ""))
+                    hasil = (amount * nilai_beli) * 1000
+                print(colored(f"Jumlah {from_currency} {format_currency(amount, from_currency)} = {to_currency} {format_currency(hasil, to_currency)}", "green" if from_currency == "IDR" else "blue"))
             else:
-                print("Mohon masukkan angka yang valid.")
+                print(colored("Mohon masukkan angka yang valid.", "red"))
     else:
-        print("Mohon masukkan pilihan yang valid.")
+        print(colored("Mohon masukkan pilihan yang valid.", "red"))
 
 
 url = "https://www.klikbca.com"
@@ -94,19 +90,31 @@ for row in kurs_rows:
         nilai_jual = cols[2].text.strip()
         table_rows.append([mata_uang, nilai_beli, nilai_jual])
 
+
+# Membuat banner
+banner_text = pyfiglet.figlet_format("Kurs BCA ^_^")
+colored_banner = colored(banner_text, "green")
+
+print(colored_banner)
+
+
 while True:
-    print("Silakan pilih opsi:")
-    print("1. Tampilkan semua kurs")
-    print("2. Calculator kurs")
-    print("3. Exit")
-    choice = input("Masukkan pilihan Anda: ")
-    if choice.isdigit() and 1 <= int(choice) <= 3:
+    print(colored("Silakan pilih opsi:", "yellow"))
+    print(colored("1. Tampilkan semua kurs", "cyan"))
+    print(colored("2. Calculator kurs", "cyan"))
+    print(colored("3. Exit", "cyan"))
+    choice = input(colored("Masukkan pilihan Anda: ", "magenta"))
+    if choice.isdigit() and 1 <= int(choice):
         choice = int(choice)
+        
         if choice == 1:
             print_table([["Mata Uang", "Nilai Beli", "Nilai Jual"]] + table_rows)
+            
         elif choice == 2:
             calculator(table_rows)
+            
         else:
             break
+            
     else:
-        print("Mohon masukkan pilihan yang valid.")
+        print(colored("Mohon masukkan pilihan yang valid.", "red"))
